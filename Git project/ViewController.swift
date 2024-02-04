f3ff4ead2548947c4cc84c0b23d2cee4f53986bd
Liza Zaharchuk <lizaza290106@gmail.com>
2024-02-04 23:06:17 +0300

    Initial Commit

---
+++ Git project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Git project
+//
+//  Created by Macbook Air on 04.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
